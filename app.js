ad26ad85f2cd8592484e17211b707be91029d1a5
cfan310 <cfanning43@gmail.com>
2022-10-16 17:11:36 -0700

    added variables for octaves C4-C5

--- app.js
+++ app.js
@@ -11,32 +11,6 @@
 // also fretboard learning mdoe, where it keeps track of your right and wrong answers and reports back to you which ones you need to work on
 // ^research best algorithm for keeping track of right and wrong data
 
-// on click show and hide keyboard innerText note names
-const showBtn = document.getElementById('showBtn');
-const hideBtn = document.getElementById('hideBtn');
-
-// add sound file for each key; play on click
-// then add sound files for every fret (hard-code at first, then use a loop)
-// then add asynch await so one plays after another (if the user is in that particular mode of play)
-
-// hard code at first; loop next?
-
-const c4key = document.querySelector('.C4-key');
-var c4audio = new Audio('sound files/C4.mp3');
-
-c4key.addEventListener('click', function () {
-  c4audio.play();
-});
-
-const d4key = document.querySelector('.D4-key');
-var d4audio = new Audio('sound files/D4.mp3');
-
-d4key.addEventListener('click', function () {
-  d4audio.play();
-});
-
-const e5key = document.querySelector('.e5key'); //
-
 // continue ading sounds, then finish the fretboard mouseover, then add fretboard sound, then add sound-for-sound logic, then add fretboard game logic
 
 /*
@@ -49,3 +23,59 @@ function showNotes() {
 
 showBtn.addEventListener('click', showNotes);
 */
+
+// on click show and hide keyboard innerText note names
+const showBtn = document.getElementById('showBtn');
+const hideBtn = document.getElementById('hideBtn');
+
+// add sound file for each key; play on click
+// then add sound files for every fret (hard-code at first, then use a loop)
+// then add asynch await so one plays after another (if the user is in that particular mode of play)
+
+// hard code at first; loop next?
+
+// C4
+const C4key = document.querySelector('.C4-key');
+const Db4key = document.querySelector('.Db4-key');
+const D4key = document.querySelector('.D4-key');
+const Eb4key = document.querySelector('.Eb4-key');
+const E4key = document.querySelector('.E4-key');
+const F4key = document.querySelector('.F4-key');
+const Gb4key = document.querySelector('.Gb4-key');
+const G4key = document.querySelector('.G4-key');
+const Ab4key = document.querySelector('.Ab4-key');
+const A4key = document.querySelector('.A4-key');
+const Bb4key = document.querySelector('.Bb4-key');
+const B4key = document.querySelector('.B4-key');
+const C5key = document.querySelector('.C5-key');
+const Db5key = document.querySelector('.Db5-key');
+const D5key = document.querySelector('.D5-key');
+const Eb5key = document.querySelector('.Eb5-key');
+const E5key = document.querySelector('.E5-key');
+const F5key = document.querySelector('.F5-key');
+const Gb5key = document.querySelector('.Gb5-key');
+const G5key = document.querySelector('.G5-key');
+const Ab5key = document.querySelector('.Ab5-key');
+const A5key = document.querySelector('.A5-key');
+const Bb5key = document.querySelector('.Bb5-key');
+const B5key = document.querySelector('.B5-key');
+
+const c4audio = new Audio('sound files/C4.mp3');
+c4key.addEventListener('click', function () {
+  c4audio.play();
+});
+// Db4
+const db4audio = new Audio('sound files/Db4.mp3');
+db4key.addEventListener('click', function () {
+  d4audio.play();
+});
+// D4
+const d4audio = new Audio('sound files/D4.mp3');
+d4key.addEventListener('click', function () {
+  d4audio.play();
+});
+// Eb4
+const eb4audio = new Audio('sound files/Eb4.mp3');
+eb4key.addEventListener('click', function () {
+  eb4audio.play();
+});
