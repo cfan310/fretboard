038eda4b6dd1f42bbcec09f5e58d775b9a6b49e6
cfan310 <cfanning43@gmail.com>
2022-10-25 13:53:58 -0700

    removed bottom comment

--- app.js
+++ app.js
@@ -374,3 +374,22 @@ C4GuitarNote.addEventListener('mouseover', playC4Guitar);
 // ^not playing atm on mouseover
 
 // import rest of gutiar files and add asynch sound
+
+function sumAll(arr) {
+  let min, max;
+  if (arr[0] > arr[1]) {
+      max = arr[0]; 
+      min = arr[1]; 
+  } else {
+      max = arr[1];
+      min = arr[0];   
+  }
+  
+  let sum = 0; 
+  for (let i = min; i < max; i++) {
+      sum += i;
+  }
+  return sum;
+}
+
+console.log(sumAll([1, 4]));
