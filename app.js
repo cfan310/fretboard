b08c710538b386175279a01cc2b71c73fac6a445
cfan310 <cfanning43@gmail.com>
2022-10-26 12:52:18 -0700

    began coding js for dynamic guitar fretboard / string creation

--- app.js
+++ app.js
@@ -375,21 +375,7 @@ C4GuitarNote.addEventListener('mouseover', playC4Guitar);
 
 // import rest of gutiar files and add asynch sound
 
-function sumAll(arr) {
-  let min, max;
-  if (arr[0] > arr[1]) {
-      max = arr[0]; 
-      min = arr[1]; 
-  } else {
-      max = arr[1];
-      min = arr[0];   
-  }
-  
-  let sum = 0; 
-  for (let i = min; i < max; i++) {
-      sum += i;
-  }
-  return sum;
-}
 
-console.log(sumAll([1, 4]));
+// DYNAMIC FRETBOARD 
+// Create element dynamically with javascript
+// Begin with div 
